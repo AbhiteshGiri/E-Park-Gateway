c3a0f4a6ca3479dcb1f1340cc8b7cec1dfac743c
Abhitesh Giri <abhiteshgiri325@gmail.com>
2024-11-05 15:51:23 +0530

    Added privacy page

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const app=express();
 const path=require("path");
 const port=8000;
 
+
 app.use(express.static(path.join(__dirname,"../public")));
+
 app.get('/',(req,res)=>{
-    res.send("serving the project E Park Gateway from Express server");
+     res.send("serving the project E Park Gateway from Express server");
 });
+
 app.listen(port,()=>{
     console.log(`Listening on port number ${port}`)
 })
